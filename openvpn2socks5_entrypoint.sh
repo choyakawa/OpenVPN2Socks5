90cbd236f38dea56365fda2b2c4d652e021a9eb5
choyakawa <97655818+choyakawa@users.noreply.github.com>
2024-11-06 07:04:21 +0800

    Update openvpn2socks5_entrypoint.sh

--- openvpn2socks5_entrypoint.sh
+++ openvpn2socks5_entrypoint.sh
@@ -4,4 +4,4 @@ nohup /usr/local/bin/entrypoint.sh &
 # Start Socks5 Server
 sleep 5s
 pubIP=$(curl ipinfo.io/ip)
-proxy socks -t tcp -p "0.0.0.0:4080" --udp-port $udpPort --udp -g $pubIP
+gost -L socks5://:${socks5port}?udp=true
