7fe6a770ce0b3c4a423f88f7a6b3ce3513a10627
choyakawa <97655818+choyakawa@users.noreply.github.com>
2024-11-20 10:13:53 +0800

    Update openvpn2socks5_entrypoint.sh

--- openvpn2socks5_entrypoint.sh
+++ openvpn2socks5_entrypoint.sh
@@ -5,6 +5,6 @@ nohup /usr/local/bin/entrypoint.sh &
 sleep 5s
 pubIP=$(curl ipinfo.io/ip)
 
-gost -L "udp://:2408/162.159.192.1:2408?keepalive=true&ttl=25s&readBufferSize=4096"
+# gost -L "udp://:2408/162.159.192.1:2408?keepalive=true&ttl=25s&readBufferSize=4096"
 gost -L socks5://:1080?udp=true
 
